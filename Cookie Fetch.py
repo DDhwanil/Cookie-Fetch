6a2c9186781cced7816e64885c2d5b434a803455
root <root@localhost.localdomain>
2015-10-08 13:48:32 +0530

    Version 2

--- Cookie Fetch.py
+++ Cookie Fetch.py
@@ -5,10 +5,15 @@ url=raw_input('Enter url (In form of http:// or https://) :http')
 url='http'+url
 no=int(raw_input('Enter the number of time to fetch cookie :'))
 a=annobrowser(proxies=[],user_agents=[('User-agent','whatever')])
-for aa in range(no):
-    a.anonymize()
-    res=a.open(url)
-    print '\n'
-    for cooki in a.cookie_jar:
-        print cooki
-    print '\n'
+try:
+    for aa in range(no):
+        a.anonymize()
+        res=a.open(url)
+        print '\n'
+        for cooki in a.cookie_jar:
+            print cooki
+        print '\n'
+except Exception, e:
+
+    if 'HTTP Error 403: Forbidden' in str(e):
+        print "Firewall Block or Url not accessible"
